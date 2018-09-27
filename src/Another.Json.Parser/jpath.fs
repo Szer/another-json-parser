eb29371931d7148c5adf5d4a6547d3d75f0176c9
Ayrat Hudaygulov <ayrat.hudaygulov@arkadium.com>
2018-09-27 19:28:31 +0300

    its alive!

--- src/Another.Json.Parser/jpath.fs
+++ src/Another.Json.Parser/jpath.fs
@@ -2,56 +2,154 @@ module JPath
 
 open FParsec
 
+let (<!>) (p: Parser<_,_>) label : Parser<_,_> =
+    fun stream ->
+        printfn "%A: Entering %s" stream.Position label
+        let reply = p stream
+        printfn "%A: Leaving %s (%A)" stream.Position label reply.Status
+        reply
+
+
+type Expression =
+    | NullLiteral
+    | NumberLiteral of double
+    | StringLiteral of string
+    | JPathLiteral of string list
+    | Paren of expr: Expression
+    | And of left: Expression * right: Expression
+    | Or of left: Expression * right: Expression
+    | Not of expr: Expression
+    | Greater of left: Expression * right: Expression
+    | Less of left: Expression * right: Expression
+    | Equal of left: Expression * right: Expression
+
+let isControlChar c =
+    ".[]<>=! @'()\""
+    |> Seq.exists (fun ch -> ch = c)
+
+let str = pstring
+let stringLiteral =
+    manySatisfy (not << isControlChar)
+
+let ws = spaces
+
+// let expr, exprRef = createParserForwardedToRef()
+// let bw p = ws >>. p .>> ws
+// let nullLit = str "null"  >>% NullLiteral
+// let numberLit = pfloat |> bw |>> NumberLiteral  <!> "number"
+// let strLit =between (str "'") (str "'") stringLiteral |> bw |>> StringLiteral <!> "str"
+// let jPathLit = str "@." >>. sepBy stringLiteral (str ".") |> bw |>> JPathLiteral <!> "jpath"
+// let paren = between (str "(") (str ")") expr |> bw |>> Paren
+
+// let op opList f =
+//     bw expr >>= fun expr1 ->
+//         opList
+//         |> List.map str
+//         |> choice
+//         |> bw
+//         |> fun x -> x >>. bw expr
+//         |>> fun expr2 -> f(expr1, expr2)
+
+// let andOp = op ["&&"; "&"] And
+// let orOp = op ["&&"; "&"] Or
+// let grOp = op [">"] Greater <!> "greater"
+// let lessOp = op ["<"] Less
+// let equalp = op ["=";"=="] Equal
+// let notOp = bw (str "!" >>. expr) |>> Not
+
+// do exprRef := choice [
+//     nullLit
+//     numberLit
+//     strLit
+//     jPathLit
+//     paren
+//     andOp 
+//     orOp
+//     grOp
+//     lessOp
+//     equalp
+//     notOp
+// ]
+
+let str_ws s = pstring s >>. ws
+
+let bw p = ws >>. p .>> ws
+let nullLit = str "null"  >>% NullLiteral
+let numberLit = pfloat |> bw |>> NumberLiteral
+let strLit =between (str "'") (str "'") stringLiteral |> bw |>> StringLiteral
+let jPathLit = str "@." >>. sepBy stringLiteral (str ".") |> bw |>> JPathLiteral
+
+let terminal = choice [
+    nullLit
+    numberLit
+    strLit
+    jPathLit
+]
+
+let opp = new OperatorPrecedenceParser<_,_,_>()
+let expr = opp.ExpressionParser
+opp.TermParser <- terminal <|> between (str_ws "(") (str_ws ")") expr
+
+opp.AddOperator(InfixOperator(">", ws, 1, Associativity.Left, fun e1 e2 -> Greater(e1,e2)))
+opp.AddOperator(InfixOperator("<", ws, 1, Associativity.Left, fun e1 e2 -> Less(e1,e2)))
+opp.AddOperator(InfixOperator("=", ws, 1, Associativity.Left, fun e1 e2 -> Equal(e1,e2)))
+opp.AddOperator(InfixOperator("==", ws, 1, Associativity.Left, fun e1 e2 -> Equal(e1,e2)))
+opp.AddOperator(InfixOperator(">=", ws, 1, Associativity.Left, fun e1 e2 -> Not(Less(e1,e2))))
+opp.AddOperator(InfixOperator("<=", ws, 1, Associativity.Left, fun e1 e2 -> Not(Greater(e1,e2))))
+opp.AddOperator(InfixOperator("&&", ws, 1, Associativity.Left, fun e1 e2 -> And(e1,e2)))
+opp.AddOperator(InfixOperator("&", ws, 1, Associativity.Left, fun e1 e2 -> And(e1,e2)))
+opp.AddOperator(InfixOperator("|", ws, 1, Associativity.Left, fun e1 e2 -> Or(e1,e2)))
+opp.AddOperator(InfixOperator("||", ws, 1, Associativity.Left, fun e1 e2 -> Or(e1,e2)))
+opp.AddOperator(PrefixOperator("!", ws, 4, true, Not))
+
+let completeExpression = ws >>. expr .>> ws
+
 type Indexer = 
     | TakeAll
-    | Filter of expression: string
-    | Slice of start: int option * finish: int option * step: int option
+    | Filter of expression: Expression
     | Union of int list
 
 type JPath =
     | Root of rest: JPath
     | Child of name: string * rest: JPath
     | RecursiveChild of name: string * rest: JPath
-    | Array of index: Indexer * rest: JPath    
+    | Array of index: Indexer * rest: JPath
+    | End
 
-let jPathElementParser, jPathElementParserRef = createParserForwardedToRef()
+let jPathElement, jPathElementRef = createParserForwardedToRef()
+
+let endOfPath : Parser<_,_> = fun stream ->
+    if stream.IsEndOfStream then Reply End
+    else Reply(Error, messageError "expected eof")
+
+let takeAll = str "*" >>% TakeAll
+let union = sepBy pint32 (str ",") |>> Union
+let filter =
+    between (str "?(") (str ")") completeExpression |>> Filter
+
+let indexer = choice [
+    filter
+    takeAll
+    union
+]
 
 let childName f =
-    let isBracket = pstring "["
-    let isDot = pstring "."
-    let endOfName = isBracket <|> isDot
-    manyChars anyChar .>>? endOfName >>= fun childName ->
-        jPathElementParser |>> fun rest -> f (childName, rest)
+    stringLiteral >>= fun childName ->
+        jPathElement |>> fun rest -> f (childName, rest)
 
-let indexer = manyChars anyChar >>% TakeAll
+let root     = str "$" >>. (jPathElement |>> Root)
+let recChild = str ".." >>. childName RecursiveChild
+let child    = str "." >>. childName Child
+let array    = between (str "[") (str "]") indexer >>= fun index ->
+    jPathElement |>> fun rest -> Array(index, rest)
 
-let ws   = spaces // eats any whitespace
-let root = pstring "$" >>. (jPathElementParser |>> Root)
-let recChild = pstring ".." >>. childName RecursiveChild
-let child = pstring "." >>. childName Child
-let array = between (pstring "[") (pstring "]") indexer >>= fun index ->
-    jPathElementParser |>> fun rest -> Array(index, rest)
-
-
-do jPathElementParserRef :=
+do jPathElementRef :=
     choice [
+        root
         recChild
         child
         array
+        endOfPath
     ]
 
-let jpath = root >>. jPathElementParser .>> eof
-
-let parse = FParsec.CharParsers.run jpath
-
-//parse "$.abc.bcd[]..e"
-
-// let parse = FParsec.CharParsers.run json
-
-// let result = parse "{abc:1}"
-
-// let jpathStart = str "$"
-// let recursive = str ".."
-// let child = str "."
-// let jPathLevel = recursive <|> child
-
+let parse = FParsec.CharParsers.run root
