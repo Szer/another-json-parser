19ce89687546f77c25087626f76632653a570ee5
Codechanger <tobasil@gmail.com>
2018-10-06 18:27:02 +0300

    Some more tests

--- src/Tests/Sample.fs
+++ src/Tests/Sample.fs
@@ -10,7 +10,7 @@ let create name (jpath,expected:JPath) = test name{
          |Success(Root actual,_,_)->Expect.equal actual expected "Wrong structure parsed"
          |_->Expect.isFalse true "Parse failed"
 }
-let cases =[
+let positiveCases =[
    ("$",End),"should parse root"
    ("$.a",JPath.Child("a",End)),"should parse with object property"
    ("$..a",JPath.RecursiveChild("a",End)), "should parse with recursive child"
@@ -18,10 +18,13 @@ let cases =[
    ("$..a..b",JPath.RecursiveChild("a",JPath.RecursiveChild("b",End))), "should parse recursive child with recursive child name"
    ("$..a.b",JPath.RecursiveChild("a",JPath.Child("b",End))), "should parse recursive child with child name"
    ("$[*]",Array (TakeAll,End)), "should take all elements from array"
+   ("$[?(@.a>@.b)]",Array(Filter (Expression.Greater (JPathLiteral ["a"],JPathLiteral ["b"])),End)) , "should parse operation with jpath literals"
+   ("$[?(@.a>5)]",Array(Filter (Expression.Greater (JPathLiteral ["a"],Expression.NumberLiteral 5.0)),End)) , "should parse operation with jpath literal and number literal"
+   ("$[?(@.a='abc')]",Array(Filter (Expression.Equal (JPathLiteral ["a"],Expression.StringLiteral "abc" )),End)) , "should parse operation with jpath literal and string literal"
   ]
 [<Tests>]
 let tests =
-  cases
+  positiveCases
    |>List.map (fun (p,n)->create n p)
    |>testList "basic tests"
     
